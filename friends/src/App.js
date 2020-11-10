559d0d226403894629503beb33fb9886e71d5f3c
Daniel Vargas <davarg5@yahoo.com>
2020-11-10 15:28:19 -0500

    set up app and started working on login

---
+++ friends/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+
+import Login from './components/Login';
+
+
+function App() {
+  return (
+    <div className="App">
+      
+
+    </div>
+  );
+}
+
+export default App;
