d1e2df261e42c4509b121bc4522e9063bad4fc61
Daniel Vargas <davarg5@yahoo.com>
2020-11-10 21:08:29 -0500

    finished mvp

--- friends/src/App.js
+++ friends/src/App.js
@@ -2,14 +2,31 @@ import './App.css';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
 import Login from './components/Login';
+import FriendsList from './components/FriendsList';
+
+import PrivateRoute from './components/PrivateRoute';
 
 
 function App() {
-  return (
-    <div className="App">
-      
 
-    </div>
+
+  return (
+    <Router>
+      <div className="App">
+        
+        <Link to='/login'>Login</Link>
+
+        <Link to='/protected'>Protected Page</Link>
+
+
+
+        <Switch>
+          <Route path='/login' component={Login} />
+          <PrivateRoute exact path='/protected' component={FriendsList} />
+          <Route component={Login} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -4,9 +4,62 @@ import axios from 'axios';
 class Login extends React.Component {
     state = {
         credentials: {
-            usename: '',
+            username: '',
             password: ''
-        }
+        },
+        isLoading: false
+    }
+
+    
+    handleChange = e => {
+        this.setState({
+            credentials: {...this.state.credentials,
+            [e.target.name]:e.target.value }
+        })
+    }
+
+    login = e => {
+        e.preventDefault(); 
+        this.setState({...this.state,
+            isLoading: true
+        });
+        axios.post(`http://localhost:5000/api/login`, this.state.credentials)
+            .then(req => {
+                localStorage.setItem('token', req.data.payload);
+                this.props.history.push('/protected');
+                this.setState({...this.state,
+                    isLoading: false
+                });
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.isLoading ? <p>Logging in...</p> : null}
+                <form onSubmit={this.login}>
+                    Username
+                    <input
+                    name='username'
+                    type='text'
+                    value={this.state.credentials.username}
+                    onChange={this.handleChange}
+                    />
+                    Password
+                    <input
+                    name='password'
+                    type='password'
+                    value={this.state.credentials.password}
+                    onChange={this.handleChange}
+                    />
+                    <button>Submit</button>
+                </form>
+            </div>
+        )
     }
 }
 
+export default Login;
